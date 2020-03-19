8e21eb35f8fdd0c3c68a57a2d33171146ad129d9
Roger N. Alegret <roger.nicolas.alegret@gmail.com>
2020-03-19 21:39:22 +0100

    First beta version ready

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A simple way to install new languages to Autocorrect Spelling on Mac
 
 Installation
 
-curl -L https://rnicolas.github.io/spelling | bash
+curl -L http://www.alegret.tech/spelling | bash
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # Heavily adapted from the pivpn.io
 #
-# curl -L https://rnicolas.github.com/spelling | bash
+# curl -L http://www.alegret.tech/spelling | bash
 # Make sure you have `curl` installed
 
 ######## SCRIPT ########
@@ -15,10 +15,40 @@ main() {
 	else
 		echo "::: You need to run this command as root"
 	fi
+    selectLanguage
+}
 
+downloadLanguage() {
     if [[! -d "/Library/Spelling/" ]]; then
     	$SUDO mkdir -p /Library/Spelling/
     fi
+    mv /Library/Spelling/
+    curl -L https://raw.githubusercontent.com/rnicolas/spelling/master/ + $1 + / + $1 + .dic
+    curl -L https://raw.githubusercontent.com/rnicolas/spelling/master/ + $1 + / + $1 + .aff
+    if [[$2]]; then
+        curl -L https://raw.githubusercontent.com/rnicolas/spelling/master/ + $1 + /hyph_ + $2 + .dic
+    else
+        curl -L https://raw.githubusercontent.com/rnicolas/spelling/master/ + $1 + /hyph_ + $1 + .dic
+    fi
 }
 
-main "$@"
+selectLanguage() {
+    echo "::: Which language do you want to install?"
+    read language
+    case "$language" in
+    "ca" | "cat" | "catala" | "català" | "catalan")
+        downloadLanguage "ca"
+        ;;
+    "va" | "val" | "valencia" | "valencià" | "valencian")
+        downloadLanguage "ca-valencia" "ca"
+        ;;
+    "oc" | "occitan" | "occità")
+        downloadLanguage "oc"
+    *)
+        echo "language not found"
+        ;;
+    esac
+
+}
+
+main
